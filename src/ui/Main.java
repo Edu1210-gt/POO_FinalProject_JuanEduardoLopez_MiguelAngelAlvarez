f9dca05a25b4dcaf8ce8f9484cf666ffb27073a9
Miguel Angel <miguel.alvarez53645@ucaldas.edu.co>
2025-11-28 09:53:28 -0500

    Final Version Main.java

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,19 +1,17 @@
 package src.ui;
 
-import javax.management.modelmbean.ModelMBeanAttributeInfo;
-
 import src.data.StoreData;
-import src.domain.Movie;
 import src.domain.Store;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        String path = "src/data/store.dat";
         Store store = new Store();
+        store = StoreData.loadStore(path);
         Menu menu = new Menu(store);
-
         menu.startMenu();
+        StoreData.saveStore(store, path);
 
     }
 
