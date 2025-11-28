28ba4b7bdb7cba43443b4df29f47d5ef3fb97768
Miguel Angel <miguel.alvarez53645@ucaldas.edu.co>
2025-11-28 09:52:37 -0500

    comments missing

--- src/domain/Store.java
+++ src/domain/Store.java
@@ -5,26 +5,38 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Store implements Serializable{
+public class Store implements Serializable {
     private static final long serialVersionUID = 1L;
-    
+
     private ArrayList<Movie> movies;
     private ArrayList<Customer> customers;
     private ArrayList<Rental> rentals;
-    
 
     public Store() {
         this.movies = new ArrayList<>();
         this.customers = new ArrayList<>();
         this.rentals = new ArrayList<>();
     }
-//________________________CRUD MOVIE____________________________________________________
+
+    // ________________________CRUD
+    // MOVIE____________________________________________________
     public boolean addMovie(Movie movie) {
-        if (findMovieById(movie.getMovieId()) == null) {
-            return movies.add(movie);
+        if (movie == null)
+            return false;
+
+        try {
+            if (movie.getMovieId() == null)
+                return false;
+            if (findMovieById(movie.getMovieId()) == null) {
+                return movies.add(movie);
+            }
+        } catch (Exception e) {
+            return false;
         }
+
         return false;
     }
+
     public Movie findMovieById(String movieId) {
         for (Movie movie : movies) {
             if (movie.getMovieId().equals(movieId)) {
@@ -32,12 +44,14 @@ public class Store implements Serializable{
             }
         }
         return null;
-    }   
+    }
+
     public List<Movie> searchMoviesByTitle(String title) {
         return movies.stream()
                 .filter(m -> m.getTitle().toLowerCase().contains(title.toLowerCase()))
                 .collect(Collectors.toList());
     }
+
     public List<Movie> searchMoviesByGenre(String genre) {
         return movies.stream()
                 .filter(m -> m.getGenre().toLowerCase().contains(genre.toLowerCase()))
@@ -49,185 +63,254 @@ public class Store implements Serializable{
                 .filter(m -> m.isAvailable())
                 .collect(Collectors.toList());
     }
-     public boolean updateMovie(String movieId, Movie updatedMovie) {
+
+    public boolean updateMovie(String movieId, Movie updatedMovie) {
+        if (movieId == null || updatedMovie == null)
+            return false;
+
         Movie movie = findMovieById(movieId);
+
         if (movie != null) {
-            movie.setTitle(updatedMovie.getTitle());
-            movie.setGenre(updatedMovie.getGenre());
-            movie.setRentalPrice(updatedMovie.getRentalPrice());
-            return true;
+            try {
+                movie.setTitle(updatedMovie.getTitle());
+                movie.setGenre(updatedMovie.getGenre());
+                movie.setRentalPrice(updatedMovie.getRentalPrice());
+                return true;
+            } catch (Exception e) {
+                return false;
+            }
         }
         return false;
     }
+
     public boolean deleteMovie(String movieId) {
+        if (movieId == null)
+            return false;
+
         Movie movie = findMovieById(movieId);
-        if (movie != null) {
-            return movies.remove(movie);
+
+        try {
+            if (movie != null) {
+                return movies.remove(movie);
+            }
+        } catch (Exception e) {
+            return false;
         }
+
         return false;
     }
-    
+
     public ArrayList<Movie> getAllMovies() {
         return movies;
     }
 
-//_____________________________CRUD CUSTOMER_______________________
+    // _____________________________CRUD CUSTOMER_______________________
 
-public boolean addCustomer(Customer customer){
-    if(findCustomerById(customer.getCustomerId())==null){
-        
-        return customers.add(customer);
-    }
-    return false;
-}
+    public boolean addCustomer(Customer customer) {
+        if (customer == null)
+            return false;
 
-public Customer findCustomerById(String customerId){
-    for(Customer c : customers){
-        if(c.getCustomerId().equals(customerId)){
-            return c;
-        }
-    }
-    return null;
-}
-
-public List<Customer> searchCustomersByName(String name){
-    return customers.stream()
-            .filter(c -> c.getName().toLowerCase().contains(name.toLowerCase()))
-            .collect(Collectors.toList());
-}
-
-public boolean updateCustomer(String customerId, Customer updateCustomer){
-    Customer customer = findCustomerById(customerId);
-    if(customer != null){
-        customer.setName(updateCustomer.getName());
-        customer.setEmail(updateCustomer.getEmail());
-        customer.setPhoneNumber(updateCustomer.getPhoneNumber());
-        return true;
-    }
-return false;
-}
-
-public boolean deleteCustomer(String customerId){
-    Customer customer = findCustomerById(customerId);
-
-    if(customer != null){
-        return customers.remove(customer);
-    }
-    return false;
-}
-
-public ArrayList<Customer> getAllCustomers(){
-    return customers;
-}
-
-//_________________________CRUD RENTAL______________________________________________
-public boolean rentMovie(Rental rental){
-    Movie movie = rental.getMovie();
-    if(movie.isAvailable()){
-        movie.setAvailable(false);
-        return rentals.add(rental);
-    }
-    return false;
-}
-public boolean returnMovie(String rentalId){
-    Rental rental = findRentalById(rentalId);
-    if(rental != null){
-        rental.getMovie().setAvailable(true);
-        return rentals.remove(rental);
-    }
-    return false;
-}
-
-public Rental findRentalById(String rentalId){
-    for(Rental r : rentals){
-        if(r.getIdRental().equals(rentalId)){
-            return r;
-        }
-    }
-    return null;
-}
-public List<Rental> getRentalsByCustomer(String customerId){
-    return rentals.stream()
-            .filter(r -> r.getCustomer().getCustomerId().equals(customerId))
-            .collect(Collectors.toList());
-        
-}
-public ArrayList<Rental> getAllRentals(){
-    return rentals;
-}
-
-//________________________REGISTER___________________________________
-public Movie leastMovie(){
-    if(rentals.isEmpty()) 
-        return null;
-    Movie least = null;
-    int min = Integer.MAX_VALUE;
-    for(Movie m : movies){
-        int count = 0;
-        for(Rental r : rentals){
-            if(r.getMovie().getMovieId().equals(m.getMovieId()))
-                count ++;
-            
-            if (count < min){
-                min = count;
-                least = m;
+        try {
+            if (customer.getCustomerId() == null)
+                return false;
+            if (findCustomerById(customer.getCustomerId()) == null) {
+                return customers.add(customer);
             }
-
+        } catch (Exception e) {
+            return false;
         }
-    }
-    return least;
-}
-public void showMoviesWithRentCount(){
-    for(Movie movie : movies){
-        int count = 0;
 
-        for (Rental r : rentals){
-            if(r.getMovie().getMovieId().equals(movie.getMovieId())){
-                count++;
+        return false;
+    }
+
+    public Customer findCustomerById(String customerId) {
+        for (Customer c : customers) {
+            if (c.getCustomerId().equals(customerId)) {
+                return c;
             }
         }
-        System.out.println("Title: " + movie.getTitle() + " | Rentals: " + count);
-    }
-}
-public Movie mostRenta(){
-
-    if(rentals.isEmpty()){
         return null;
     }
 
-    Movie most = null;
-    int max = 0;
+    public List<Customer> searchCustomersByName(String name) {
+        return customers.stream()
+                .filter(c -> c.getName().toLowerCase().contains(name.toLowerCase()))
+                .collect(Collectors.toList());
+    }
 
-    for(Movie m : movies){
-        int count = 0;
-        for(Rental r : rentals){
-            if(r.getMovie().getMovieId().equals(m.getMovieId())){
-            count++;
+    public boolean updateCustomer(String customerId, Customer updateCustomer) {
+        if (customerId == null || updateCustomer == null)
+            return false;
+
+        Customer customer = findCustomerById(customerId);
+
+        if (customer != null) {
+            try {
+                customer.setName(updateCustomer.getName());
+                customer.setEmail(updateCustomer.getEmail());
+                customer.setPhoneNumber(updateCustomer.getPhoneNumber());
+                return true;
+            } catch (Exception e) {
+                return false;
+            }
         }
-        
+        return false;
     }
-    if(count > max){
-        max = count;
-        most = m;
+
+    public boolean deleteCustomer(String customerId) {
+        if (customerId == null)
+            return false;
+
+        Customer customer = findCustomerById(customerId);
+
+        try {
+            if (customer != null) {
+                return customers.remove(customer);
+            }
+        } catch (Exception e) {
+            return false;
+        }
+
+        return false;
     }
-}
-return most;
-}
 
+    public ArrayList<Customer> getAllCustomers() {
+        return customers;
+    }
 
+    // _________________________CRUD
+    // RENTAL______________________________________________
+    public boolean rentMovie(Rental rental) {
+        if (rental == null)
+            return false;
 
+        try {
+            Movie movie = rental.getMovie();
+            if (movie == null)
+                return false;
 
+            if (movie.isAvailable()) {
+                movie.setAvailable(false);
+                return rentals.add(rental);
+            }
+        } catch (Exception e) {
+            return false;
+        }
 
+        return false;
+    }
 
+    public boolean returnMovie(String rentalId) {
+        if (rentalId == null)
+            return false;
 
-public void setMovies(ArrayList<Movie> movies){
-    this.movies = movies;
-}
+        Rental rental = findRentalById(rentalId);
 
-public void setCustomers(ArrayList<Customer> customers){
-    this.customers = customers;
-}
-public void setRentals(ArrayList<Rental> rentals){
-    this.rentals = rentals;
-}
+        if (rental != null) {
+            try {
+                Movie movie = rental.getMovie();
+                if (movie != null && !movie.isAvailable()) {
+                    movie.setAvailable(true);
+                    rentals.remove(rental);
+                    return true;
+                }
+            } catch (Exception e) {
+                return false;
+            }
+        }
+
+        return false;
+    }
+
+    public Rental findRentalById(String rentalId) {
+        for (Rental r : rentals) {
+            if (r.getIdRental().equals(rentalId)) {
+                return r;
+            }
+        }
+        return null;
+    }
+
+    public List<Rental> getRentalsByCustomer(String customerId) {
+        return rentals.stream()
+                .filter(r -> r.getCustomer().getCustomerId().equals(customerId))
+                .collect(Collectors.toList());
+
+    }
+
+    public ArrayList<Rental> getAllRentals() {
+        return rentals;
+    }
+
+    // ________________________REGISTER___________________________________
+    public Movie leastMovie() {
+        if (rentals.isEmpty())
+            return null;
+        Movie least = null;
+        int min = Integer.MAX_VALUE;
+        for (Movie m : movies) {
+            int count = 0;
+            for (Rental r : rentals) {
+                if (r.getMovie().getMovieId().equals(m.getMovieId()))
+                    count++;
+
+                if (count < min) {
+                    min = count;
+                    least = m;
+                }
+
+            }
+        }
+        return least;
+    }
+
+    public void showMoviesWithRentCount() {
+        for (Movie movie : movies) {
+            int count = 0;
+
+            for (Rental r : rentals) {
+                if (r.getMovie().getMovieId().equals(movie.getMovieId())) {
+                    count++;
+                }
+            }
+            System.out.println("Title: " + movie.getTitle() + " | Rentals: " + count);
+        }
+    }
+
+    public Movie mostRenta() {
+
+        if (rentals.isEmpty()) {
+            return null;
+        }
+
+        Movie most = null;
+        int max = 0;
+
+        for (Movie m : movies) {
+            int count = 0;
+            for (Rental r : rentals) {
+                if (r.getMovie().getMovieId().equals(m.getMovieId())) {
+                    count++;
+                }
+
+            }
+            if (count > max) {
+                max = count;
+                most = m;
+            }
+        }
+        return most;
+    }
+
+    public void setMovies(ArrayList<Movie> movies) {
+        this.movies = movies;
+    }
+
+    public void setCustomers(ArrayList<Customer> customers) {
+        this.customers = customers;
+    }
+
+    public void setRentals(ArrayList<Rental> rentals) {
+        this.rentals = rentals;
+    }
 }
